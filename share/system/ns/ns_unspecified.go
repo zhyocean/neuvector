5787c4cb7d3efbfe772be7dfedda66bd26c39dfd
zhanghaiyang <1125694337@qq.com>
2023-03-03 17:09:48 +0800

    init project

---
+++ share/system/ns/ns_unspecified.go
@@ -0,0 +1,24 @@
+//go:build !linux
+// +build !linux
+
+package namespace
+
+import (
+	"errors"
+)
+
+func Set(ns *NsHandle) (err error) {
+	return ErrNotImplemented
+}
+
+func get(nstype string) (NsHandle, error) {
+	return -1, ErrNotImplemented
+}
+
+func getFromPid(nstype string, proc string, pid int) (NsHandle, error) {
+	return -1, ErrNotImplemented
+}
+
+func getFromThread(nstype string, pid, tid int) (int, error) {
+	return -1, ErrNotImplemented
+}
